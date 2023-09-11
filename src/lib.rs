e8b12c9ee8968790d735d3c395546cc0355ba855
FuzzyForest <red.hope6833@fastmail.com>
2023-09-11 22:27:06 +0100

    Day 11

--- src/lib.rs
+++ src/lib.rs
@@ -25,15 +25,9 @@ impl<E: LispExpression> Atom<E> for LispString {
     where
         Self: Sized,
     {
-        if token.value.as_bytes()[0] == b'"'
-            && token.value.as_bytes()[token.value.len() - 1] == b'"'
-        {
-            String::from_utf8(token.value.as_bytes()[1..token.value.len() - 1].to_vec())
-                .ok()
-                .map(LispString)
-        } else {
-            None
-        }
+        Some(LispString(
+            token.value.strip_prefix('"')?.strip_suffix('"')?.to_owned(),
+        ))
     }
 }
 
@@ -45,55 +39,14 @@ impl Display for LispString {
 
 create_expression!(
     StringyExpression,
-    Symbol,
-    Number,
     List<StringyExpression>,
     BuiltinFunction<StringyExpression>,
     BuiltinMacro<StringyExpression>,
     Lambda<StringyExpression>,
     Macro<StringyExpression>,
-    LispString
-);
-
-impl LispExpression for StringyExpression {
-    fn as_atom(&self) -> &dyn Atom<Self> {
-        match self {
-            StringyExpression::Symbol(a) => a,
-            StringyExpression::Number(a) => a,
-            StringyExpression::List(a) => a,
-            StringyExpression::BuiltinFunction(a) => a,
-            StringyExpression::BuiltinMacro(a) => a,
-            StringyExpression::Lambda(a) => a,
-            StringyExpression::Macro(a) => a,
-            StringyExpression::LispString(a) => a,
-        }
-    }
-
-    fn parse_from_token(token: &Token) -> Self {
-        None.or_else(|| <Number as Atom<Self>>::parse_from_token(token).map(Self::from))
-            .or_else(|| <LispString as Atom<Self>>::parse_from_token(token).map(Self::from))
-            .or_else(|| <Symbol as Atom<Self>>::parse_from_token(token).map(Self::from))
-            // This will never fail as symbols never fail parsing
-            .unwrap()
-    }
-}
-
-impl Display for StringyExpression {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        self.as_atom().fmt(f)
-    }
-}
-
-impl_try_to_from!(
-    StringyExpression,
-    Symbol,
+    LispString,
     Number,
-    Lambda<StringyExpression>,
-    Macro<StringyExpression>,
-    BuiltinFunction<StringyExpression>,
-    BuiltinMacro<StringyExpression>,
-    List<StringyExpression>,
-    LispString
+    Symbol
 );
 
 pub fn split<E>(arguments: &[E], _env: &mut Environment<E>) -> Result<E>
@@ -114,26 +67,11 @@ where
     Ok(List(splits).into())
 }
 
-pub fn upper<E>(arguments: &[E], _env: &mut Environment<E>) -> Result<E>
-where
-    E: LispExpression + ToAndFrom<LispString>,
-{
-    if arguments.len() != 1 {
-        bail!("Upper needs a single argument");
-    }
-    let argument: &LispString = arguments[0]
-        .try_into_atom()
-        .context("Argument to upper must be a string")?;
+pub fn upper(argument: &LispString) -> Result<LispString> {
     Ok(LispString(argument.0.to_uppercase()).into())
 }
 
 pub fn set_environment<E: LispExpression + ToAndFrom<LispString>>(env: &mut Environment<E>) {
-    env.set(
-        Symbol("split".to_owned()),
-        BuiltinFunction::new("split", split),
-    );
-    env.set(
-        Symbol("upper".to_owned()),
-        BuiltinFunction::new("upper", upper),
-    );
+    env.set("split", BuiltinFunction::new("split", split));
+    env.set("upper", BuiltinFunction::new_wrapped("upper", upper));
 }
